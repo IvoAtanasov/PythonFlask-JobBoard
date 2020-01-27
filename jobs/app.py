ec0e4f569e6a203b978ee80e6934c72d529983f5
Ivo Ivanov Atanasov <ivoatanasov85@gmail.com>
2020-01-27 21:45:08 +0200

    Add flask

--- jobs/app.py
+++ jobs/app.py
@@ -1,12 +1,3 @@
-from flask import Flask, escape, request
+from flask import Flask,render_template
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
-
-
-if __name__ == "__main__":
-    app.run()
+app=Flask(__name__)
